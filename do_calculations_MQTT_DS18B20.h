87dad9124601f0d50e3574a8a60caa0fb6cdc173
MartinP1 <Martin.Praekelt@o2online>
2023-03-11 18:52:11 +0100

    neues file

---
+++ do_calculations_MQTT_DS18B20.h
@@ -0,0 +1,21 @@
+/**
+@brief use available input values to calculate new output values
+
+input values from global variables 
+Temp Sensor 0 -> room temperature (temp[0])
+Temp Sensor 1 -> heater inlet temp (temp[1] - optional sensor!)
+Temp Sensor 2 -> heater return temp (temp[2] - optional sensor!)
+
+desired Temperature (desired_temp)
+maximum fan speed (pwmSet)
+Window contact state (future)
+
+
+
+
+*/
+void runTempControl()
+{
+
+
+}
