bb2f8a85d9d72193d4f96e1bfa525fe0b33b90d9
Martin P <Martin.Praekelt@o2online.de>
2024-06-13 09:55:57 +0200

    Update fan_pwm_MQTT_DS18B20.h

--- fan_pwm_MQTT_DS18B20.h
+++ fan_pwm_MQTT_DS18B20.h
@@ -1,9 +1,9 @@
 
 void setSpeed(uint8_t uiSpeed) {
-#if defined(ARDUINO_D1_MINI32) || defined(ARDUINO_LOLIN_S2_MINI)
-  ledcWrite( 0, uiSpeed);
-#else
   uiSpeed = 255 - uiSpeed; // circuitry is inverting, ensure 255 = max, 0 = off
+#if defined(ARDUINO_D1_MINI32) || defined(ARDUINO_LOLIN_S2_MINI)
+  ledcWrite( pwmGpio, uiSpeed);
+#else
   analogWrite(pwmGpio, uiSpeed);
 #endif
 }
