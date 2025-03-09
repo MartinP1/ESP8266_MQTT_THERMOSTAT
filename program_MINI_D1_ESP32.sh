4e9fd27cc957e416935ff011a925656f21902e05
martin <Martin.Praekelt@o2online.de>
2025-03-09 14:34:37 +0100

    remote programming scripts updated
    intermediate not working state

--- program_MINI_D1_ESP32.sh
+++ program_MINI_D1_ESP32.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
 # installation on debiantest LXC
 #=============
-# copy from mounted data SMB share to local disc
-
+# activate venv
+source /home/martin/.local/pipx/venvs/esptool/bin/activate
 #=============
 # do programming
+# '/home/martin/.local/pipx/venvs/esptool'
 python3 "/home/martin/.local/pipx/venvs/esptool/bin/esptool.py" \
-  --chip esp32s2 --port "/dev/ttyACM0" --baud 921600  --before default_reset \
- --after hard_reset write_flash  -z --flash_mode keep --flash_freq keep --flash_size keep \
+  --chip esp32 --port "/dev/serial/by-id/usb-Silicon_Labs_CP2104_USB_to_UART_Bridge_Controller_02S7GIX3-if00-port" \
+  --baud 921600 --before default_reset \ 
+  --after hard_reset write_flash  -z --flash_mode keep --flash_freq keep --flash_size keep \
  0x1000 "./ESP8266_MQTT_THERMOSTAT.ino.bootloader.bin" \
  0x8000 "./ESP8266_MQTT_THERMOSTAT.ino.partitions.bin" \
  0xe000 "./boot_app0.bin" \
@@ -15,5 +16,5 @@ python3 "/home/martin/.local/pipx/venvs/esptool/bin/esptool.py" \
 
 # ..bootloader.bin from /home/martin/.cache/arduino/sketches/<uuid>
 # ..partition.bin from /home/martin/.cache/arduino/sketches/<uuid>
-# ..app0.bin from /home/martin/.arduino15/packages/esp32/hardware/esp32/3.0.7/tools/partitions/ (3.1.1 not working)
+# ..app0.bin from /home/martin/.arduino15/packages/esp32/hardware/esp32/3.0.7/tools/partitions/
 # ..ino.bin from /home/martin/.cache/arduino/sketches/<uuid>
--- upload_binaries.sh
+++ upload_binaries.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
-copy 
+# ..bootloader.bin from /home/martin/.cache/arduino/sketches/<uuid>
+# ..partition.bin from /home/martin/.cache/arduino/sketches/<uuid>
+# ..app0.bin from /home/martin/.arduino15/packages/esp32/hardware/esp32/3.0.7/tools/partitions/ (3.1.1 not working)
+# ..ino.bin from /home/martin/.cache/arduino/sketches/<uuid>
+
+cd /home/martin/.cache/arduino/sketches
+ls -l
+
+read -p "Welches Image: " var
+mypath="/home/martin/.cache/arduino/sketches/"$var
+echo "processing "$mypath
+
+read -p "Password auf Zielsystem: " var
+
+
+# ls -l $mypath/ESP8266_MQTT_THERMOSTAT.ino.bootloader.bin
+
+# cp $mypath/ESP8266_MQTT_THERMOSTAT.ino.bootloader.bin /mnt/data/Martin/Projekte/ARDUINO/Thermostat/ESP8266_MQTT_THERMOSTAT.ino.bootloader.bin
+# cp $mypath/ESP8266_MQTT_THERMOSTAT.ino.partitions.bin /mnt/data/Martin/Projekte/ARDUINO/Thermostat/ESP8266_MQTT_THERMOSTAT.ino.partitions.bin
+# cp $mypath/ESP8266_MQTT_THERMOSTAT.ino.bin /mnt/data/Martin/Projekte/ARDUINO/Thermostat/ESP8266_MQTT_THERMOSTAT.ino.bin
+# cp /home/martin/.arduino15/packages/esp32/hardware/esp32/3.0.7/tools/partitions/boot_app0.bin /mnt/data/Martin/Projekte/ARDUINO/Thermostat/boot_app0.bin
+# cp /home/martin/github/ESP8266_MQTT_THERMOSTAT/program_MINI_D1_ESP32.sh /mnt/data/Martin/Projekte/ARDUINO/Thermostat/program_MINI_D1_ESP32.sh
+# cp /home/martin/github/ESP8266_MQTT_THERMOSTAT/program_esp32s2.sh /mnt/data/Martin/Projekte/ARDUINO/Thermostat/program_esp32s2.sh
+
+sshpass -p $var rsync -a $mypath/ESP8266_MQTT_THERMOSTAT.ino.bootloader.bin martin@192.168.2.164:/home/martin/thermostat
+sshpass -p $var rsync -a $mypath/ESP8266_MQTT_THERMOSTAT.ino.partitions.bin martin@192.168.2.164:/home/martin/thermostat
+sshpass -p $var rsync -a $mypath/ESP8266_MQTT_THERMOSTAT.ino.bin martin@192.168.2.164:/home/martin/thermostat
+sshpass -p $var rsync -a /home/martin/.arduino15/packages/esp32/hardware/esp32/3.0.7/tools/partitions/boot_app0.bin martin@192.168.2.164:/home/martin/thermostat
+sshpass -p $var rsync -a /home/martin/github/ESP8266_MQTT_THERMOSTAT/program_MINI_D1_ESP32.sh martin@192.168.2.164:/home/martin/thermostat
+sshpass -p $var rsync -a /home/martin/github/ESP8266_MQTT_THERMOSTAT/program_esp32s2.sh martin@192.168.2.164:/home/martin/thermostat
+
